2ca85e7daa0316d486043ef2aae817bac5d36c7a
DipSapkota125 <dipsapkota325@gmail.com>
2023-05-07 17:32:57 +0545

    variables and type conversion

---
+++ index.js
@@ -0,0 +1,70 @@
+//variables
+// var x = 90;
+// x = 120;
+// var x = 7 + 9;
+// console.log(x);
+
+//for let
+// let x = 70;
+// console.log(x);
+// x = 80;
+// console.log(x);
+// let x = 100;
+// console.log(x);
+
+//const
+// const a = 75;
+// console.log(a);
+// a = 100;
+// console.log(a);
+// const a = 45;
+// console.log(a);
+
+//data type
+// const y = 90;
+// console.log(y);
+// console.log(typeof y);
+
+// const name = "Swoopma Suman";
+// console.log(name);
+// console.log(typeof name);
+
+//another way of finding datatype
+// const result = "Hari pradhan";
+
+// console.log(result);
+// console.log(typeof result);
+// console.log(typeof Number(result));
+
+// const nums = 80 + 90;
+// console.log(nums);
+// console.log(typeof nums);
+// console.log(typeof String(nums));
+
+//boolean
+// let x = 90;
+// let y = 60;
+// let z = false;
+// console.log(x + y + z);
+
+// let x = "Harka rai";
+// let y = 80;
+// console.log(x + y);
+
+// const name = "Bartika";
+// const surname = "Rai";
+// console.log(" My name is : " + name + " " + surname);
+// console.log(`My name is : ${name} ${surname}.`);
+
+// let z;
+// console.log(z);
+// console.log(typeof z);
+
+// let z = null;
+// console.log(z);
+// console.log(typeof z);
+
+//Strings method
+
+// const result = "My name is Lod Krishna!";
+// console.log(result[0]);
